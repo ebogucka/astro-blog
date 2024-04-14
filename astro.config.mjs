152bc2438e742e3115f4c3cc374e3865b747e652
Ela Bogucka <9089666+ebogucka@users.noreply.github.com>
2024-04-15 00:21:23 +0200

    Include a Preact greeting banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://beamish-bienenstitch-db14f9.netlify.app"
+  site: "https://beamish-bienenstitch-db14f9.netlify.app",
+  integrations: [preact()]
 });
